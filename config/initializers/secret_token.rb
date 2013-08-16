cbc77c2dcd805ce97d7fa31251859c04b05bfdb6
Louise Au Yeung <louise@eabx.org>
2013-08-16 07:39:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shmoogle::Application.config.secret_key_base = '4602076ef11efb983118eb0a0be01c6593fccb99e756f6292d263c388f6675a5ba9cb17b8cf961f142764e49bf507f2a80ab0b30914312bad6dcc945151fdfc0'
